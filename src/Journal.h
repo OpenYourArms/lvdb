a968c548824be9bb23bbd952ec9f072cbe2f4fe3
OpenYourArms <1773086297@qq.com>
2020-04-20 00:16:45 +0800

    添加几个journal._read相关的函数，为对象增加friend函数，方便输出调试。

--- src/Journal.h
+++ src/Journal.h
@@ -25,11 +25,20 @@ private:
     enum {FILE_ZERO_SIZE = 10};
 
     struct KeyInfo{
+        enum{KEY_INFO_HEAD_SIZE= sizeof(ULL)+ sizeof(char)};
         enum{DELETE=0,PUT=1,GET=2};
         void toBuf(char buf[],int& pos){
             toBuffer(buf,pos,keySequence);
             toBuffer(buf,pos,type);
             toBuffer(buf,pos,key);
+        }
+        friend ostream& operator<<(ostream& os,KeyInfo& keyInfo){
+            os<<"\t\t\t\t\t##########     KeyInfo   Begin     ############"<<endl;
+            os<<"KeyInfo.keySequence:\t"<<keyInfo.keySequence<<endl;
+            os<<"KeyInfo.type:(0=delete,1=put,2=get)\t"<<keyInfo.type-0<<endl;
+            os<<"KeyInfo.key:\t"<<keyInfo.key<<endl;
+            os<<"\t\t\t\t\t##########     KeyInfo   End     ############"<<endl;
+            return os;
         }
         ULL keySequence;
         char type;
@@ -45,7 +54,17 @@ private:
             toBuffer(buf,pos,valueLength);
             toBuffer(buf,pos,value);
         }
-
+        friend ostream& operator<<(ostream& os,OperationInfo& oi){
+            os<<"\t\t\t\t\t##########     OperationInfo   Begin     ############"<<endl;
+            os<<"OperationInfo.type:\t"<<oi.type-0<<endl;
+            os<<"OperationInfo.keyLength\t"<<oi.keyLength<<endl;
+            os<<"OperationInfo.KeyInfo"<<endl;
+            os<<oi.keyInfo<<endl;
+            os<<"OperationInfo.valueLength\t"<<oi.valueLength<<endl;
+            os<<"OperationInfo.value\t"<<oi.value<<endl;
+            os<<"\t\t\t\t\t##########     OperationInfo   End     ############"<<endl;
+            return os;
+        }
         char type;
         int keyLength;
         KeyInfo keyInfo;
@@ -62,7 +81,19 @@ private:
             toBuffer(buf,pos,size);
             for(auto& e:operationVector) e.toBuf(buf,pos);
         }
-
+        friend ostream& operator<<(ostream& os,LogInfo& li){
+            os<<"\t\t\t\t\t##########     LogInfo   Begin     ############"<<endl;
+            os<<"LogInfo.logSequence\t"<<li.logSequence<<endl;
+            os<<"LogInfo.count\t"<<li.count<<endl;
+            os<<"LogInfo.size\t"<<li.size<<endl;
+            os<<"LogInfo.operationVector\tsize:\t"<<li.operationVector.size()<<endl;
+            for(int no=0;no<li.operationVector.size();no++){
+                cout<<"no "<<no<<"\t";
+                os<<li.operationVector[no]<<endl;
+            }
+            os<<"\t\t\t\t\t##########     LogInfo   End     ############"<<endl;
+            return os;
+        }
         ULL logSequence;
         int count;
         int size;
@@ -89,12 +120,29 @@ private:
             logChunkHeader.toBuf(buf,pos);
             logInfo.toBuf(buf,pos);
         }
-
+        friend ostream& operator<<(ostream& os,LogChunk& lc){
+            os<<"\t\t\t\t\t##########     LogChunk   Begin     ############"<<endl;
+            os<<"LogChunk.logChunkHeader.checkSum\t"<<lc.logChunkHeader.checkSum<<endl;
+            os<<"LogChunk.logChunkHeader.length\t"<<lc.logChunkHeader.length<<endl;
+            os<<"LogChunk.logChunkHeader.type\t"<<lc.logChunkHeader.type-0<<endl;
+            os<<"LogChunk.LogInfo\t"<<endl<<lc.logInfo<<endl;
+            os<<"\t\t\t\t\t##########     LogChunk   End     ############"<<endl;
+            return os;
+        }
+        void getChunkData(vector<Data>& vcData);
         Header logChunkHeader;
         LogInfo logInfo;
     };
     struct BlockHeader{
-        enum {BLOCK_HEADER_SIZE = sizeof(ULL)+sizeof(ULL)+sizeof(int)};
+        enum {BLOCK_HEADER_SIZE = sizeof(ULL)+sizeof(ULL)+sizeof(off_t)};
+        friend ostream& operator<<(ostream& os,BlockHeader& bh){
+            os<<"\t\t\t\t\t##########     BlockHeader   Begin     ############"<<endl;
+            os<<"BlockHeader.logSequence\t"<<bh.logSequence<<endl;
+            os<<"BlockHeader.keySequence\t"<<bh.keySequence<<endl;
+            os<<"BlockHeader.nextChunkOffset\t"<<bh.nextChunkOffset<<endl;
+            os<<"\t\t\t\t\t##########     BlockHeader   End     ############"<<endl;
+            return os;
+        }
         ULL logSequence;
         ULL keySequence;
         off_t nextChunkOffset;
@@ -109,7 +157,12 @@ public:
     void _writeBlockHeaderBack();
     Data _write(OPERATION_p op);
     vector<Data> _write(vector<OPERATION_p> ops);
-    bool _read();
+    bool findBlockHeader(off_t& block,off_t& bOffset,ULL seq);
+    void readKeyInfo(off_t& block,off_t& bOffset,KeyInfo& keyInfo,int length);
+    void readOperationInfo(off_t& block,off_t& bOffset,OperationInfo& operationInfo);
+    void readLogInfo(off_t& block,off_t& bOffset,LogInfo& logInfo);
+    void readLogChunk(off_t& block,off_t& bOffset,LogChunk& logChunk);
+    vector<Data> _read(ULL seq);
 
 private:
     BlockHeader _blockHeader;
