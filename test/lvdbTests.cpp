5d8ce1290f1b0144ea28803c5399a9c00bf3e029
OpenYourArms <1773086297@qq.com>
2020-04-21 23:25:44 +0800

    添加各种测试，顺便走走google的流程

--- test/lvdbTests.cpp
+++ test/lvdbTests.cpp
@@ -66,11 +66,67 @@ TEST(SkipList,all){
     //test_SkipList();
     SkipList sl;
     vector<int> vc={1,3,8,6,2,4,9,7,5,19,22,14};
-
+    /*
+1    : 1	1	1		1    : 1	1	1		1    : 1	1	1		1    : 1	1	1		1    : 1	1	1
+14   : 14	1	14		14   : 14	1	14
+19   : 19	1	19		19   : 19	1	19		19   : 19	1	19
+2    : 2	1	2		2    : 2	1	2
+22   : 22	1	22		22   : 22	1	22		22   : 22	1	22
+3    : 3	1	3		3    : 3	1	3		3    : 3	1	3
+4    : 4	1	4		4    : 4	1	4
+5    : 5	1	5		5    : 5	1	5		5    : 5	1	5		5    : 5	1	5		5    : 5	1	5
+6    : 6	1	6		6    : 6	1	6		6    : 6	1	6		6    : 6	1	6		6    : 6	1	6
+7    : 7	1	7		7    : 7	1	7		7    : 7	1	7
+8    : 8	1	8		8    : 8	1	8
+9    : 9	1	9		9    : 9	1	9		9    : 9	1	9		9    : 9	1	9
+     *
+     * */
+    for(auto e:vc){
+        string k=to_string(e);
+        string v=k;
+        Data data(e,Data::PUT,k,v);
+        sl.insert(data);
+    }
+    SkipList::Node* path[SkipList::_MAX_HEIGHT];
+    string key1=to_string(7);
+    auto e=sl.findGreatOrEqual(key1,path);
+    ASSERT_STREQ(e->_data._key.c_str(),key1.c_str());
+    string key2="11";
+    auto e2=sl.findGreatOrEqual(key2,path);
+    ASSERT_STREQ(e2->_data._key.c_str(),"14");
+    string key3="21",key4="2";
+    auto e3=sl.findGreatOrEqual(key4,path);
+    Data data(atoi(key3.c_str()),Data::PUT,key3,key3);
+    sl.insert(data);
+    ASSERT_TRUE(sl.keyIsAfterNode(key3,e3));
 }
 // FakeAlloc
-TEST(FakeAlloc,all){
-    EXPECT_EQ(1,1);
+class FakeAllocTest:public ::testing::Test{
+protected:
+    FakeAlloc fa;
+    void SetUp() override {
+        ;
+    }
+    void TearDown() override {
+        ;
+    }
+};
+TEST_F(FakeAllocTest,roundUp){
+    std::size_t size1=12;
+    size_t size2=fa.roundUp(size1);
+    ASSERT_EQ(size2,32);
+    size1=47;
+    size2=fa.roundUp(size1);
+    ASSERT_EQ(size2,32*2);
+}
+TEST_F(FakeAllocTest,getIndex){
+    //ASSERT_EQ(fa.getIndex(0),0);
+    ASSERT_EQ(fa.getIndex(1),0);
+    ASSERT_EQ(fa.getIndex(32),0);
+    ASSERT_EQ(fa.getIndex(33),1);
+    ASSERT_EQ(fa.getIndex(63),1);
+    ASSERT_EQ(fa.getIndex(64),1);
+    ASSERT_EQ(fa.getIndex(fa._MAX_BLOCK),fa._INDEX_SIZE-1);
 }
 // Journal
 TEST(Journal,all){
