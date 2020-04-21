a5440f1d82291605189b982d500b4c7e8813a61a
OpenYourArms <1773086297@qq.com>
2020-04-21 20:22:34 +0800

    加入googletest前保存一下

--- main.cpp
+++ main.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 #include <ctime>
+#include <cstring>
 #include "src/Journal.h"
 #include "src/SkipList.h"
 #include "src/FakeAlloc.h"
+#include "src/lvmisc.h"
 using namespace std;
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
-    test_FakeAlloc();
+    test_journal();
     return 0;
 }
