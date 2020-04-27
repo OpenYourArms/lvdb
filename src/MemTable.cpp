d057997ff3056be5a95b9bb90d63ebcc48678d9a
OpenYourArms <1773086297@qq.com>
2020-04-27 23:24:57 +0800

    修改跳表与memtable关系，原来是包含一个对象，现在使用智能指针管理跳表；跳表满则开启线程写入，写完析构，新建跳表；而且测试通过

--- src/MemTable.cpp
+++ src/MemTable.cpp
@@ -3,11 +3,12 @@
 //
 
 #include "MemTable.h"
-
+#include <future>
+extern SSTManage sstM;// 全局唯一
 int TableBase::insert(Data& data) {
     int dataSize=data.myByteSize();
     assert(getLeftSpace()>=dataSize);
-    tbList.insert(data);
+    tbList_ptr->insert(data);
     ++dataCounter;
     listByteSize+=dataSize;
     return 0;
@@ -16,6 +17,13 @@ int TableBase::insert(Data& data) {
 int TableBase::writeSSTable() {
     assert(imFlag==IMMUTABLE);
     //cout<<"SST Create!"<<endl;
+    sstM.writeToSST(tbList_ptr->getIterator());
+    auto p=tbList_ptr.release();
+    delete p;
+    tbList_ptr.reset(new SkipList());
+    listByteSize=0;
+    dataCounter=0;
+    imFlag=MUTABLE;
 }
 
 int MemTable::findTable() {
@@ -35,9 +43,24 @@ int MemTable::saveData(Data &data) {
         }else{
             tb.setNextStatus();
             // save SST
-            tb.writeSSTable();// new thread to write SkipList data.
+            // todo 应该修改状态，开线程去写
+            auto trd=async(std::launch::async,&TableBase::writeSSTable,&tb);// new thread to write SkipList data.
             pos=findTable();
         }
     }
     return pos;
+}
+
+void testMemTable(){
+    // 先去main,定义全局变量sstM;  SSTManage sstM=SSTManage("/tmp/tmpp/SSTManage");
+    MemTable mt;
+    int N=1000;
+    for(int i=0;i<N;i++){
+        Data data(i,1,to_string(i),to_string(i));
+        auto e=mt.saveData(data);
+        if(e==-1){
+            cout<<"### \t\t"<<i<<endl;
+            break;
+        }
+    }
 }
--- src/MemTable.h
+++ src/MemTable.h
@@ -9,16 +9,18 @@
 
 #include "lvmisc.h"
 #include "SkipList.h"
+#include "SSTManage.h"
+#include <memory>
 
 struct TableBase{//一个 memTable
     enum {MUTABLE=true,IMMUTABLE=false};
     // todo size最大值待设置
     enum {TABLE_MAX_SIZE=100};
-    SkipList tbList;
+    unique_ptr<SkipList> tbList_ptr;
     int listByteSize;
     bool imFlag;
     int dataCounter;
-    TableBase():listByteSize(0),imFlag(MUTABLE),dataCounter(0){}
+    TableBase():tbList_ptr(new SkipList()),listByteSize(0),imFlag(MUTABLE),dataCounter(0){}
     inline int getLeftSpace(){return TABLE_MAX_SIZE-listByteSize;}
     inline void setNextStatus(){imFlag=!imFlag;}
     int insert(Data& data);// 返回码对应字符车串
