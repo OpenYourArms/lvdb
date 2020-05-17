dd23ad2581a1665e3c3f5eaf0ba9fa061b13bd05
OpenYourArms <1773086297@qq.com>
2020-05-17 19:48:13 +0800

    测试对比性能相关

--- main.cpp
+++ main.cpp
@@ -39,6 +39,10 @@ struct Test{
 };*/
 // SSTManage sstM=SSTManage("/tmp/tmpp/SSTManage");
 extern SSTManage sstM;
+int ok=0;
+void prt(){
+    cout<<"ok\t"<<ok<<endl;
+}
 int main() {
     std::cout << "Hi, World!" << std::endl;
 //    testSSTable(1);
@@ -50,15 +54,56 @@ int main() {
 //    testSSTableFind();
 //    test_SSTManage_with_SSTable();
 
-    vector<OPERATION_p> vc;
-    int caseNum=20;
-    for(int i=0;i<caseNum;i++){
-        int op1=Data::GET;
-        string key=to_string(1000+i);
-        cout<<key<<endl;
-        vc.push_back({op1,{key,key}});
+    atexit(prt);
+    //初始化
+    string journalPath="/tmp/tmpp/log.txt";
+    Journal journal(journalPath);
+    MemTable mt;
+    // 存在在日志而不在文件的数据；
+    if(!journal.empty()){
+        ULL lastSeq=sstM.getMxSequenceNumber();
+        //lastSeq为已存储的最大seq, _read寻找大于等于lastSeq.故+1
+        vector<Data> unRecord=journal._read(lastSeq+1);
+        cout<<"unRecord.size\t"<<unRecord.size()<<endl;
+        for(auto& e:unRecord){
+            if(e._op!=Data::GET){// get操作对memTable无用
+                mt.saveData(e);
+            }
+        }
     }
 
+    // 新数据
+    string key="";
+    int tms=10;
+    int N=100000;
+
+    int base=1000;
+    clock_t t0,t1,sumt=0;
+    cout<<"test begin"<<endl;
+    sumt=0;
+    for(int k=0;k<tms;k++){
+        t0=clock();
+        for(int i=0;i<N;i++){
+//            int op1=Data::GET;
+            int op1=rand()%3;
+//            cout<<k<<"***"<<i<<endl;
+//            key=to_string(base+i+(k*10));
+            key=to_string(base+i);
+            auto data=journal._write({op1,{key,key}});
+            // 处理操作
+            if(op1!=Data::GET){
+                int rtNumber=mt.saveData(data);
+            }else{
+                Data rtData;
+                rtData=mt.findGreatOrEqual(data);
+                if(rtData._key!=data._key){
+                    rtData=sstM.findGreatOrEqual(data);
+                }
+            }
+        }
+        sumt+=clock()-t0;
+    }
+    cout<<"put\t"<<N<<"\tcost time:\t"<<(sumt+1)/tms<<"\t=\t"<<((sumt+1)/tms)/(1000000)<<"s"<<endl;
 
 //    cout<<"READ JOURNAL..."<<endl;
 //    auto vcData=journal._read(0);
